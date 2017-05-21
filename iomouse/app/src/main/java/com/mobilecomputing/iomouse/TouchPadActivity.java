83400565d87a14f91adbee67ca198be5a1685ab1
Jacob Andersen <Jacob.Secher.Andersen@gmail.com>
2017-05-21 12:11:54 +0200

    Added on touch on double click

--- iomouse/app/src/main/java/com/mobilecomputing/iomouse/TouchPadActivity.java
+++ iomouse/app/src/main/java/com/mobilecomputing/iomouse/TouchPadActivity.java
@@ -1,17 +1,23 @@
 package com.mobilecomputing.iomouse;
 
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 
-public class TouchPadActivity extends AppCompatActivity {
+public class TouchPadActivity extends AppCompatActivity implements
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener{
 
+    private String DEBUG_TAG     = "ABC";
     private int xPrevious;
     private int yPrevious;
     private long timeStamp; // updated time stamp whenever activity is touched
-
     private float sensitivity; // should be a value in shared preferences
+
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,13 @@ public class TouchPadActivity extends AppCompatActivity {
         yPrevious = 0;
         timeStamp = System.currentTimeMillis();
         sensitivity = 1.2f;
+
+        mDetector = new GestureDetectorCompat(this,this);
+        // Set the gesture detector as the double tap
+        // listener.
+        mDetector.setOnDoubleTapListener(this);
+
+
     }
 
     // called whenever the screen is touched within the activity
@@ -51,6 +64,7 @@ public class TouchPadActivity extends AppCompatActivity {
             MouseCommands.moveMouse(dx, dy);
         }
 
+/*
         switch(event.getAction()) {
 
             case MotionEvent.ACTION_DOWN:
@@ -74,14 +88,73 @@ public class TouchPadActivity extends AppCompatActivity {
 
                 return false;
         }
+*/
 
 
-        return false;
+        this.mDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
     }
+
+
 
     private int scale(int i){
 
         return Math.round(i*sensitivity);
 
+    }
+
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        // on touch
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        // when touch ends
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+                            float distanceY) {
+        // on movement (not two finger scroll)
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        Log.d("ABC", "onShowPress");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        // Log.d("ABC", "onSingleTapUp");;
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        Log.d("ABC", "onDoubleTab");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        Log.d("ABC", "onDoubleTabEvent");
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        // Log.d("ABC", "onSingleTapConfirmed");
+        return true;
     }
 }
